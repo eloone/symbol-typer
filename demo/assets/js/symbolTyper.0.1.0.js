801f6d8d6e70130a6cd1d9dae73ea51c5f1ee254
eloone <elodie.rafali@gmail.com>
2014-06-04 01:09:36 +0200

    removed demo from master

--- demo/assets/js/symbolTyper.0.1.0.js
+++
@@ -1,5 +0,0 @@
-/*
-Elodie Rafalimanana Copyright 2014 - BSD License 
-I dedicate this script to the fans of the Wingdings font, something to reminisce about our childhood.
-symbolTyper - v0.1.0 - 2014-06-04
-*/
--- demo/helpers/raw.js
+++
@@ -1,5 +0,0 @@
-module.exports.register = function (Handlebars, options)  { 
-  Handlebars.registerHelper("raw", function(options) {
-  	return options.fn(this).replace(/\[\[/g, '{{').replace(/\{\{\[/g, '{{{').replace(/\]\]/g, '}}').replace(/\}\}\]/g, '}}}');
-  });
-};
